7b7ab35b23e06a997ec2fbe4d0c92d687c57a866
keanlouis30 <keanlouis30@gmail.com>
2024-02-18 16:23:09 +0800

    header chaNGES

--- header.js
+++ header.js
@@ -10,5 +10,34 @@ document.addEventListener("DOMContentLoaded", function() {
     const searchBarHeight = getComputedStyle(searchBar).height;
     searchButton.style.height = searchBarHeight;
 
-   
+    const dropbutton = document.getElementById("dropdownbutton");
+    const optionsdiv = document.getElementById("optionsdiv");
+    dropbutton.addEventListener("mouseenter", function(){
+        optionsdiv.classList.toggle("show");
+    });
+
+    window.onclick = function(event) {
+        if (!event.target.matches('#dropdownbutton')) {
+            var dropdowns = document.getElementsByClassName("dropdowncontent");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+
+    const profilebutton = document.getElementById("profilebutton");
+    const settingsbutton = document.getElementById("settingsbutton");
+    
+    
+    settingsbutton.addEventListener("click", function(){
+        window.location.href = 'settings.html';
+    });
+    profilebutton.addEventListener("click", function(){
+        window.location.href = 'userprofile_rosales.html';
+    });
+
 });
--- mco_home_registeredjs.js
+++ mco_home_registeredjs.js
@@ -1,31 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (){
-    const profilebutton = document.getElementById("profilebutton");
-    const settingsbutton = document.getElementById("settingsbutton");
-    const dropbutton = document.getElementById("dropdownbutton");
-    const optionsdiv = document.getElementById("optionsdiv");
     
-    settingsbutton.addEventListener("click", function(){
-        window.location.href = 'settings.html';
-    });
-    profilebutton.addEventListener("click", function(){
-        window.location.href = 'userprofile_rosales.html';
-    });
-
-    dropbutton.addEventListener("mouseenter", function(){
-        optionsdiv.classList.toggle("show");
-    });
-
-    window.onclick = function(event) {
-        if (!event.target.matches('#dropdownbutton')) {
-            var dropdowns = document.getElementsByClassName("dropdowncontent");
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                }
-            }
-        }
-    }
+    
 
 });
