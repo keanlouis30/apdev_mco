445714046334943f6ac436307d01da045fa4219c
keanlouis30 <keanlouis30@gmail.com>
2024-02-19 21:08:45 +0800

    changes to settings

--- settjs.js
+++ settjs.js
@@ -11,17 +11,27 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const passwordButton = document.getElementById("sb2");
     const profileButton = document.getElementById("sb1");
+    const appearanceButton = document.getElementById("sb3");
     const profilepane = document.getElementById("profilerightpane1");
-    const passwordpane = document.getElementById("profilerightpane1");
+    const passwordpane = document.getElementById("profilerightpane2");
+    const appearancepane = document.getElementById("profilerightpane3");
     
     passwordButton.addEventListener("click", function(){
-        passwordpane.style.zIndex = 2;
+        passwordpane.style.zIndex = 3;
         profilepane.style.zIndex = 1;
+        appearancepane.style.zIndex = 2;
     });
 
     profileButton.addEventListener("click", function(){
         passwordpane.style.zIndex = 1;
+        profilepane.style.zIndex = 3;
+        appearancepane.style.zIndex = 2;
+    });
+
+    appearanceButton.addEventListener("click", function(){
+        passwordpane.style.zIndex = 1;
         profilepane.style.zIndex = 2;
+        appearancepane.style.zIndex = 3;
     });
 
     const dp = document.getElementById("dpimg");
@@ -30,6 +40,16 @@ document.addEventListener("DOMContentLoaded", function() {
     imagechangebutton.onchange = function(){
         dp.src = URL.createObjectURL(imagechangebutton.files[0]);
     };
+
+    const darkmode = document.getElementById("dmodetoggle");
+    const html = document.documentElement;
+    const header = document.getElementById("header");
+
+    darkmode.addEventListener("click", function(){
+        html.style.backgroundImage = 'none';
+        html.style.backgroundColor = 'var(--black)';
+        header.style.backgroundColor = 'var(--black)';
+    });
 
     
 });
